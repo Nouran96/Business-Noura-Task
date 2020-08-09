9f2c99d0fa5503bcca2c208fee0951cc2db90514
Nouran Samy <nourans.96@yahoo.com>
2020-08-09 23:26:47 +0200

    Main Side tabs

---
+++ assets/js/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        mouseDrag: false,
+        touchDrag: false,
+        nav: true,
+        navText: ['<span class="fas fa-angle-left"></span>', '<span class="fas fa-angle-right"></span>'],
+        dots: false,
+        items: 1
+    });
+});
