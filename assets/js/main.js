3d1fffa9d7980e34a13b1014cbca88b8cae660f5
Nouran Samy <nourans.96@yahoo.com>
2020-08-10 13:22:21 +0200

    Carousel with corresponding tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 $('.tab').click(function() {
+    // Add active class to the active tab and remove from all other tabs
     $('.tab').removeClass('active');
     $(this).addClass('active');
+
+    // Get the class name of the active tab and display the corresponding carousel
+    var carouselOrder = this.className.split(' ')[1];
+    $('.owl-carousel').addClass('d-none');
+    $(`.owl-carousel.${carouselOrder}`).removeClass('d-none');
 });
